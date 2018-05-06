d10fcac176c96df04abefa87765d5dc90efbd759
Juan Ocampo <david.ocampo.3928@gmail.com>
2018-05-06 17:44:29 -0500

    Login Page

---
+++ Russia/Russia/ViewModels/LoginViewModel.cs
@@ -0,0 +1,100 @@
+﻿namespace Russia.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    public class LoginViewModel : BaseViewModel
+    {
+        #region Attributes
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
+        #region Properties
+        public string Email
+        {
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+        }
+
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+            this.IsEnabled = true;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "error",
+                    "EmailValidation",
+                    "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "error",
+                    "EmailValidation",
+                    "Accept");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+
+
+
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            this.Email = string.Empty;
+            this.Password = string.Empty;
+            #endregion
+        }
+    }
+}
---
+++ Russia/Russia/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿namespace Russia.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            instance = this;
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance;
+
+        public static MainViewModel GetInstance()
+        {
+            if (instance == null)
+            {
+                return new MainViewModel();
+            }
+
+            return instance;
+        }
+        #endregion
+    }
+}
