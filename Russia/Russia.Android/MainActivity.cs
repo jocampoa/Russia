6d30884d3308fe9d0e5a3a1e467e48560517fde7
Juan Ocampo <david.ocampo.3928@gmail.com>
2018-05-20 11:27:42 -0500

    Splash

--- Russia/Russia.Android/MainActivity.cs
+++ Russia/Russia.Android/MainActivity.cs
@@ -1,15 +1,15 @@
-﻿using System;
-
-using Android.App;
-using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
-
-namespace Russia.Droid
+﻿namespace Russia.Droid
 {
-    [Activity(Label = "Russia", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    using Android.App;
+    using Android.Content.PM;
+    using Android.OS;
+
+    [Activity(
+        Label = "Russia", 
+        Icon = "@drawable/icon", 
+        Theme = "@style/MainTheme", 
+        MainLauncher = false, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
